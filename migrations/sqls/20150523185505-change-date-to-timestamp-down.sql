856c5a4feaad17ba15ff09b0fef08fa84fa4b50b
Sean Walker <swlkr.rbl@gmail.com>
2015-05-23 11:57:51 -0700

    Use timestamp column not date columns for created_at

---
+++ migrations/sqls/20150523185505-change-date-to-timestamp-down.sql
@@ -0,0 +1,3 @@
+alter table visits alter created_at type date;
+alter table users alter created_at type date;
+alter table sites alter created_at type date;
---
+++ migrations/sqls/20150523185505-change-date-to-timestamp-up.sql
@@ -0,0 +1,3 @@
+alter table visits alter created_at type timestamp;
+alter table users alter created_at type timestamp;
+alter table sites alter created_at type timestamp;
