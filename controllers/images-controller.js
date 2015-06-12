aa9b0d8dc86842c6723c3163eac20b7d8a98d85a
Sean Walker <swlkr.rbl@gmail.com>
2015-06-11 20:03:28 -0700

    Set cache-control header to midnight of the next day

--- controllers/images-controller.js
+++ controllers/images-controller.js
@@ -12,9 +12,14 @@ const ImagesController = {
     // Create a visit
     yield Visit.create(filename.slice(0, -4), this.request);
 
+    // Cache control
+    var now = new Date();
+    var midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0);
+    var secondsUntilMidnight = Math.floor((midnight.getTime() - now.getTime())/1000);
 
     this.type = "image/gif";
     this.body = yield Image.read();
+    this.set("Cache-Control", "public, max-age=" + secondsUntilMidnight);
   }
 };
 
