0de465b78bac3a54ad38a0a9430e9c91a1bdbace
Sean Walker <swlkr.rbl@gmail.com>
2015-06-23 20:59:28 -0700

    Add mailgun configs

--- config.js
+++ config.js
@@ -2,6 +2,11 @@ require("dotenv").load();
 
 var config = {};
 
+config.mailgun = {};
+config.mailgun.apiKey = process.env.MAILGUN_API_KEY;
+config.mailgun.domain = process.env.MAILGUN_DOMAIN || "sandboxc5bc765ef14841d3bdf61ff755c12bd5.mailgun.org";
+config.mailgun.from = process.env.MAILGUN_FROM || "postmaster@sandboxc5bc765ef14841d3bdf61ff755c12bd5.mailgun.org";
+
 config.db = {};
 config.db.url = process.env.DATABASE_URL || "postgres://postgres:@localhost:5432/visics_development";
 
